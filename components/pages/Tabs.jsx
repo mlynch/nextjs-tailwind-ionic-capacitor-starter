dbf5f4ad6f0d4ef8f81be33a6be9adc5e901a5a5
Max Lynch <max@ionic.io>
2021-01-25 11:18:43 -0600

    Fix menu routing

--- components/pages/Tabs.jsx
+++ components/pages/Tabs.jsx
@@ -10,31 +10,29 @@ import Settings from './Settings';
 
 const Tabs = () => {
   return (
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tabs/feed" component={Home} exact={true} />
-          <Route path="/tabs/lists" component={Lists} exact={true} />
-          <Route path="/tabs/lists/:listId" component={ListDetail} exact={true} />
-          <Route path="/tabs/settings" component={Settings} exact={true} />
-          <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tabs/feed">
-            <IonIcon icon={flash} />
-            <IonLabel>Feed</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tabs/lists">
-            <IonIcon icon={list} />
-            <IonLabel>Lists</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tabs/settings">
-            <IonIcon icon={cog} />
-            <IonLabel>Settings</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route path="/tabs/feed" component={Home} exact={true} />
+        <Route path="/tabs/lists" component={Lists} exact={true} />
+        <Route path="/tabs/lists/:listId" component={ListDetail} exact={true} />
+        <Route path="/tabs/settings" component={Settings} exact={true} />
+        <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} />
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="tab1" href="/tabs/feed">
+          <IonIcon icon={flash} />
+          <IonLabel>Feed</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab2" href="/tabs/lists">
+          <IonIcon icon={list} />
+          <IonLabel>Lists</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab3" href="/tabs/settings">
+          <IonIcon icon={cog} />
+          <IonLabel>Settings</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
   );
 };
 
