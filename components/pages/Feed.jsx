67890999c798e020c019af1a666a943c99cc1c21
Max Lynch <max@ionic.io>
2022-10-02 09:47:08 -0500

    Fix images. Closes #31

--- components/pages/Feed.jsx
+++ components/pages/Feed.jsx
@@ -21,7 +21,7 @@ import Store from '../../store';
 const FeedCard = ({ title, type, text, author, authorAvatar, image }) => (
   <Card className="my-4 mx-auto">
     <div className="h-32 w-full relative">
-      <Image className="rounded-t-xl" objectFit="cover" src={image} alt="" layout='fill' />
+      <img className="rounded-t-xl object-cover min-w-full min-h-full max-w-full max-h-full" src={image} alt="" />
     </div>
     <div className="px-4 py-4 bg-white rounded-b-xl dark:bg-gray-900">
       <h4 className="font-bold py-0 text-s text-gray-400 dark:text-gray-500 uppercase">{type}</h4>
@@ -29,7 +29,7 @@ const FeedCard = ({ title, type, text, author, authorAvatar, image }) => (
       <p className="sm:text-sm text-s text-gray-500 mr-1 my-3 dark:text-gray-400">{text}</p>
       <div className="flex items-center space-x-4">
         <div className="w-10 h-10 relative">
-          <Image layout='fill' src={authorAvatar} className="rounded-full" alt="" />
+          <img src={authorAvatar} className="rounded-full object-cover min-w-full min-h-full max-w-full max-h-full" alt="" />
         </div>
         <h3 className="text-gray-500 dark:text-gray-200 m-l-8 text-sm font-medium">{author}</h3>
       </div>
