9cd34f99940283b201be02cc667ee98ba999df0f
Emmanuel Ah-kouen <emmanuel.ahkouen@gmail.com>
2022-10-20 12:28:15 +0900

    Bumped TailwinCSS from 2 to 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  purge: [],
+  content: [    
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'media',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
