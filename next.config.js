ef343fe400a7d8750502cd69dd16d8a7f0f25616
nirk4m <nirk@4m-a.com>
2023-03-16 17:21:09 +0200

    pwa integration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withTM = require('next-transpile-modules')([
-  '@ionic/react',
-  '@ionic/core',
-  '@stencil/core',
-  'ionicons',
-]);
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
-module.exports = withTM({
+module.exports = withPWA({
   basePath: '',
   images: {
     domains: ['images.unsplash.com'],
   },
+  transpilePackages: ['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons'],
   swcMinify: true,
 });
