3b726d689205df78a01455790011fa698a6914df
Max Lynch <max@ionic.io>
2023-04-06 10:42:43 -0500

    Update to Next.js 13 and Ionic 7

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-const withTM = require('next-transpile-modules')([
-  '@ionic/react',
-  '@ionic/core',
-  '@stencil/core',
-  'ionicons',
-]);
-
-module.exports = withTM({
+module.exports = {
   basePath: '',
   images: {
     domains: ['images.unsplash.com'],
   },
   swcMinify: true,
-});
+  transpilePackages: ['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons'],
+}
