51234a2d5ed353da822dca013414dcd10f5e3ff2
Or Sam <orfacemailj@gmail.com>
2023-04-06 17:12:49 +0300

    change the layout to ion and started form validation

--- components/pages/CreateTale/index.tsx
+++ components/pages/CreateTale/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import {
+  IonCard,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonDatetimeButton,
+  IonModal,
+  IonDatetime,
   IonPage,
   IonHeader,
   IonToolbar,
@@ -9,76 +16,108 @@ import {
   IonIcon,
   IonContent,
   IonMenuButton,
-  IonItem,
-  IonInput,
 } from '@ionic/react';
 
 import Card from '../../ui/Card';
 
 const CreateTale = () => {
   const [tripName, setTripName] = useState('');
+  const [isNameValid, setIsNameValid] = useState(false);
   const [catchphrase, setCatchphrase] = useState('');
-  const [coverPhoto, setCoverPhoto] = useState('');
+  const [isCatchphraseValid, setIsCatchphraseValid] = useState(false);
   const [startDate, setStartDate] = useState('');
+  const [isStartDateValid, setIsStartDateValid] = useState(false);
   const [endDate, setEndDate] = useState('');
+  const [isEndDateValid, setIsEndDateValid] = useState(false);
 
-  const onButtonClick = (e) => {
-    e.preventDefault();
-    console.log(`tripName: ${tripName}, catchphrase: ${catchphrase}`);
-    //need to send an axios call to server and create a new trip
+  const tripNameChangeHandler = e => {
+    setTripName(e.target.value);
+  };
+
+  const catchphraseChangeHandler = e => {
+    setCatchphrase(e.target.value);
+  };
+
+  const startDateChangeHandler = e => {
+    const formattedDate = formatDate(e.target.value);
+
+    setStartDate(formattedDate);
+  };
+
+  const endDateChangeHandler = e => {
+    const formattedDate = formatDate(e.target.value);
+
+    setEndDate(formattedDate);
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    return `${day}-${month}-${year}`;
+  };
+
+  const createTaleHandler = () => {
+    console.log("Submit process started");
+    validateForm();
+    console.log("Submit process ended");
+  }
+
+  const validateForm = () => {
+    setIsNameValid(tripName ? true : false);
+    setIsNameValid(catchphrase ? true : false);
+    //setIsStartDateValid((startDate &&) ? true : false); //
   }
 
   return (
     <Card className="my-4 mx-auto">
-      <form style={{ padding: '2vh 2vw', display: 'grid' }}>
-        <div className="tripData">
-          <div className="description" style={{ display: 'flex', flexDirection: 'column' }}>
-            <label htmlFor="tripName">Trip Name</label>
-            <input
-              type="text"
-              className="form-control"
+      <IonCard className="my-4 mx-auto">
+        <form style={{ padding: '2vh 2vw', display: 'grid' }}>
+          <IonItem fill="outline">
+            <IonLabel position="floating">Trip Name</IonLabel>
+            <IonInput
+              placeholder="Enter Trip Name..."
               id="tripName"
-              style={{ marginBottom: '1vh', border: '1px solid #b8c1cc', borderRadius: '5px' }}
-              onChange={e => setTripName(e.target.value)}
+              onIonChange={tripNameChangeHandler}
+              value={tripName}
             />
-            <label htmlFor="catchphrase">Catchphrase</label>
-            <input
-              type="text"
-              className="form-control"
+          </IonItem>
+          <IonItem fill="outline">
+            <IonLabel position="floating">Catchphrase</IonLabel>
+            <IonInput
+              placeholder="Enter Catchphrase..."
               id="catchphrase"
-              style={{ marginBottom: "1vh", border: "1px solid #b8c1cc", borderRadius: "5px" }}
-              onChange={e => setCatchphrase(e.target.value)}
+              onIonChange={catchphraseChangeHandler}
+              value={catchphrase}
             />
-            {/*<label htmlFor="coverPhoto">Cover Photo</label>
-            <input
-              type="text"
-              className="form-control"
-              id="coverPhoto"
-              style={{ marginBottom: "1vh", border: "1px solid #b8c1cc", borderRadius: "5px" }}
-              onChange={e => setCoverPhoto(e.target.value)}
-            /> should check if we want to add this field at this part of the form*/}
-          </div>
-          <div className="dates" style={{ display: 'flex', flexDirection: 'column' }}>
-            <label htmlFor="startDate">Start Date</label>
-            <input
-              type="date"
-              className="form-control"
-              id="startDate"
-              style={{ marginBottom: "1vh", border: "1px solid #b8c1cc", borderRadius: "5px" }}
-              onChange={e => setStartDate(e.target.value)}
-            />
-            <label htmlFor="endDate">End Date</label>
-            <input
-              type="date"
-              className="form-control"
-              id="endDate"
-              style={{ marginBottom: "1vh", border: "1px solid #b8c1cc", borderRadius: "5px" }}
-              onChange={e => setEndDate(e.target.value)}
-            />
-          </div>
-        </div>
-        <button style={{ margin: "1vh 1vw", border: "1px solid #b8c1cc", borderRadius: "5px" }} onClick={onButtonClick} >Create Tale</button>
-      </form>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Start Date</IonLabel>
+            <IonDatetimeButton datetime="startDatetime"></IonDatetimeButton>
+            <IonModal keepContentsMounted={true}>
+              <IonDatetime
+                id="startDatetime"
+                presentation="date"
+                onIonChange={startDateChangeHandler}
+              ></IonDatetime>
+            </IonModal>
+          </IonItem>
+          <IonItem>
+            <IonLabel>End Date</IonLabel>
+            <IonDatetimeButton datetime="endDatetime"></IonDatetimeButton>
+            <IonModal keepContentsMounted={true}>
+              <IonDatetime
+                id="endDatetime"
+                presentation="date"
+                onIonChange={endDateChangeHandler}
+              ></IonDatetime>
+            </IonModal>
+          </IonItem>
+          <IonButton onClick={createTaleHandler}>Create Your Tale</IonButton>
+        </form>
+      </IonCard>
     </Card>
   );
 };
