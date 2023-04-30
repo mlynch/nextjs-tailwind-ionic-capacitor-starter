40cc7acdf57b2404da91e567ce850349e0aa77dd
nirk4m <nirklinger471@gmail.com>
2023-05-01 00:32:10 +0300

    image tape

--- components/pages/TaleOverview/Story/index.tsx
+++ components/pages/TaleOverview/Story/index.tsx
@@ -14,9 +14,10 @@ import {
 } from '@ionic/react';
 import { StoryResponse } from '../../../../types/types';
 import { parseDuration } from '../../../../utils/converters';
-import { time } from 'ionicons/icons';
+import { time, todayOutline } from 'ionicons/icons';
 import PostgresInterval from 'postgres-interval';
 import ImageTape from '../../../ui/ImageTape';
+import { Activity } from './Activity';
 
 type StoryProps = {
   story: StoryResponse;
@@ -30,31 +31,20 @@ function Story({ story }: StoryProps) {
           .filter(act => act.destination_id === dest.id)
           .sort((act1, act2) => act1.sequential_number - act2.sequential_number);
         const activities = destActivities.map((act, index) => (
-          <div
-            className={
-              'border border-gray-400 rounded-md bg-blue-50 lg:shadow-lg md:shadow-lg px-4 mx-2 py-4'
-            }
-            key={act.id}
-          >
-            <div className={'flex flex-row gap-2 text-lg font-medium'}>
-              <IonLabel>{act.name}</IonLabel>
-              <div className={'flex flex-row gap-1 items-center'}>
-                <IonLabel>
-                  {parseDuration(act.duration as PostgresInterval.IPostgresInterval)}
-                </IonLabel>
-                <IonIcon color={'primary'} icon={time} />
-              </div>
-            </div>
-            <div className={'mb-4'}>
-              <p>{act.description}</p>
-            </div>
-            <ImageTape media={act.media} />
-          </div>
+          <Activity key={act.id} activity={act} />
         ));
         return (
           <IonAccordion key={dest.id} value={dest.name}>
             <IonItem slot="header">
               <h1>{dest.name}</h1>
+              <div
+                className={'h-full -mb-4 flex flex-row items-center gap-1 mx-4 text-lg font-medium'}
+              >
+                <span className={'underline italic'}>
+                  days: {dest.first_day}-{dest.last_day}
+                </span>
+                <IonIcon color={'tertiary'} icon={todayOutline} />
+              </div>
             </IonItem>
 
             <div className="flex flex-col gap-8 bg-gray-50 py-4" slot="content">
--- components/ui/ImageTape.tsx
+++ components/ui/ImageTape.tsx
@@ -3,14 +3,22 @@ import { MediaType } from '../../types/db-schema-definitions';
 import Image from 'next/image';
 
 function ImageTape({ media }: { media: { media_type: MediaType | null; media_url: string }[] }) {
+  if (!media.length) return;
   return (
-    <section className="flex flex-row overflow-scroll gap-2 h-60">
+    <div className="flex flex-row overflow-scroll gap-2 w-full h-60 ">
       {media.map(media => (
-        <div key={media.media_url} className="rounded-md overflow-hidden relative w-60">
-          <Image fill style={{ objectFit: 'cover' }} src={media.media_url} alt={media.media_type} />
+        <div key={media.media_url} className={'flex-shrink-0 w-full md:w-60 lg:w-80 h-full'}>
+          <div className=" rounded-md overflow-hidden relative w-full h-full">
+            <Image
+              fill
+              style={{ objectFit: 'cover' }}
+              src={media.media_url}
+              alt={media.media_type}
+            />
+          </div>
         </div>
       ))}
-    </section>
+    </div>
   );
 }
 
